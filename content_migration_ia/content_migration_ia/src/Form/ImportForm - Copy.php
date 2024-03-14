9a5e051b8081f399522fcc850e86a64d1853fddb
stikadia <33320096+stikadia@users.noreply.github.com>
2024-03-14 16:21:27 +0530

    Changes in export and import logic

---
+++ content_migration_ia/content_migration_ia/src/Form/ImportForm - Copy.php
@@ -0,0 +1,582 @@
+<?php
+
+
+namespace Drupal\content_migration_ia\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\File\FileSystemInterface;
+use Drupal\file\Entity\File;
+use Drupal\taxonomy\Entity\Term;
+use Drupal\Core\Url;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\node\Entity\Node;
+use Drupal\Core\Render\BubbleableMetadata;
+use Drupal\Core\Utility\Token;
+use Drupal\path_alias\Entity\PathAlias;
+use Drupal\media\Entity\Media;
+use Drupal\media\Entity\MediaType;
+
+/**
+ * Migrate CSV form
+ */
+
+class ImportForm extends FormBase{
+
+	protected $msgObj, $filesystem, $entityTypeManager, $file_url_generator;
+	protected $allowlangs = [];
+
+	public function __construct() {
+		$this->msgObj = \Drupal::messenger();
+		$this->filesystem = \Drupal::service("file_system");
+		$this->entityTypeManager = \Drupal::service('entity_type.manager');
+		$this->file_url_generator = \Drupal::service('file_url_generator');
+
+		$languageManager = \Drupal::service("language_manager");
+		$languages = $languageManager->getLanguages();
+	
+		foreach ($languages as $language) {
+			$this->allowlangs[] = $language->getId();
+		}
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getFormId(){
+		return 'import_form';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function buildForm(array $form, FormStateInterface $form_state){
+		
+		$content_type_storage = $this->entityTypeManager->getStorage('node_type');
+		$content_type_entities = $content_type_storage->loadMultiple();
+
+		
+		$content_types = [''=>"Select Content Type"];
+		foreach ($content_type_entities as $content_type) {
+			$content_types[$content_type->id()] = $content_type->label();
+		}
+		
+		$form['contenttype'] = [
+			'#type' => "select",
+			'#title' => 'Content Type',
+			'#required' => true,
+			'#description' => 'Select content type where you want to import the csv data',
+			'#options' => $content_types,
+		];
+		$form['filename'] = [
+			'#type' => "managed_file",
+			//'#type' => "file",
+			"#title" => $this->t("Upload CSV File"),
+			//"#required" => TRUE,
+			"#description" => "eg., Select Blog CSV file and click on upload to save data in blog content type of Drupal.",
+			'#upload_location' => 'public://importdata',
+			'#upload_validators' => [
+			    'file_validate_extensions' => ['csv'],
+			],
+			 
+			//"#multiple" => true,
+			"#accept"=>"text/csv", 
+		];
+
+		$form["#attributes"] = ['enctype' => 'multipart/form-data'];
+
+		
+		$form['submit'] = [
+			'#type' => "submit",
+			"#value" => "Import",
+		];
+		
+
+		return $form;
+	}
+
+	
+	/**
+	 * {@inheritdoc}
+	 */
+	public function submitForm(array &$form, FormStateInterface $form_state){
+		
+		$form_files = $form_state->getValue('filename');
+		$contenttype = $form_state->getValue('contenttype','');
+		
+		$msgObj = $this->msgObj;
+			
+		//echo "<pre>"; print_r($_POST); exit;
+
+		foreach($form_files as $form_file)
+		{
+			if (isset($form_file) && !empty($form_file)) {
+			
+			  $file = File::load($form_file);
+			  $file_path = $file->getFileUri();
+			  $path = \Drupal::service('file_url_generator')->generateAbsoluteString($file_path);
+              
+			  $csv_data = $this->processCSVFile($file_path);
+			  // Set up the batch process.
+			  $batch = [
+					'title' => t('Importing CSV Data'),
+					'operations' => [],
+					'init_message' => t('Starting import...'),
+					'progress_message' => t('Processed @current out of @total.'),
+					'finished' => [$this, 'importFinished'],
+			  ];
+
+			  $batch_size = 1; // Adjust the batch size as needed.
+
+			$chunks = array_chunk($csv_data, $batch_size);
+			//dump($chunks);
+			//dump($csv_data);
+			//die();
+			foreach ($chunks as $index => $chunk) {
+				$batch['operations'][] = [[$this, 'processChunk'], [$chunk, $contenttype, $index]];
+			}
+
+			batch_set($batch);
+			  
+			$file->delete();
+			  
+			 
+			  
+			}
+		}
+		
+		$messages = $msgObj->all();
+
+		if(!isset($messages['error']))
+		{
+			$msgObj->addMessage(t("Data imported successfully !!"));
+		}
+		
+	}
+
+	public function processChunk($chunk, $contenttype, $index, &$context) {
+		$drupalRoot = \Drupal::root()."/";
+		foreach($chunk as $values)
+		{
+			if(isset($values["nid"]) && $values["nid"] > 0)
+			{
+				$node = Node::load($values["nid"]);
+				if (!($node && $node->bundle() === $contenttype)) {
+					$msgObj->addError(t("Node id, you are trying to update is not of selected content type."));
+					break;
+				}
+				if(isset($values["langcode"]))
+				{
+					$langcode = $values["langcode"];
+					
+					if(!in_array($langcode,$this->allowlangs))
+					{
+						$msgObj->addError(t("Node id, you are trying to update is not valid for langcode: ".$langcode));
+						break;
+					}
+
+					$node_langcode = $node->language()->getId();
+
+					if($langcode != $node_langcode)
+					{
+						if(!$node->isTranslatable()){
+							$msgObj->addError(t("Node, you are trying to translate is not translatable"));
+							break;
+						}
+						if($node->hasTranslation($langcode)) {
+							$node = $node->getTranslation($langcode);
+						}
+						else
+						{
+							$node = $node->addTranslation($langcode);
+						}
+					}
+				}
+			}
+			else{
+				$node = Node::create(['type' => $contenttype]);
+			}
+			//die(dump($values));
+			//die(dump($node));
+
+			$fieldArr = ["path","metatag"];
+
+			foreach($values as $field_name=>$value)
+			{
+				if(in_array($field_name,$fieldArr)) { continue; }
+				if ($node->hasField($field_name)) {
+					
+					$field_definition = $node->getFieldDefinition($field_name);
+					$field_type = $field_definition->getType();
+					//echo $field_name." -- ".$field_type."<hr/>";
+
+					//dump($field_definition);
+					
+					$allValues = explode("<SEPERATOR>",$value);
+					
+					if($field_type=="image" || $field_type == 'file')
+					{
+						$directory_pattern = $field_definition->getSetting('file_directory');
+						$directory = $this->getDirectoryName($directory_pattern);
+						$fidArr = [];
+						foreach($allValues as $fileurl)
+						{
+							$file_data = @file_get_contents($fileurl);
+							if(!$file_data)
+							{
+								continue;	
+							}
+
+							$filename = basename($fileurl);
+							
+							// Save the image data as a file
+							file_put_contents($drupalRoot."sites/default/files/".$filename, $file_data);
+							
+							if($directory!='')
+							{
+								$destination = 'public://'.$directory."/".$filename;
+							}
+							else{
+								$destination = 'public://'.$filename;
+							}
+							
+							$this->filesystem->move($drupalRoot."sites/default/files/".$filename, $destination, FileSystemInterface::EXISTS_REPLACE);
+
+							// Create a file entity from the downloaded image
+							$file = \Drupal::entityTypeManager()->getStorage('file')->create([
+							'uri' => $destination,
+							]);
+
+							$file->save();
+							$fidArr[] = $file->id();
+						}
+						if(!empty($fidArr))
+						{
+							$node->get($field_name)->setValue($fidArr);
+						}
+					}
+					/*else if($field_type == "text_with_summary")
+					{
+						$default_format = $field_definition->getFieldStorageDefinition()->getSetting('default_format');
+						if(is_null($default_format))
+						{
+							$default_format = "full_html";
+						}
+
+						$node->{$field_name}->value = $value;
+						$node->{$field_name}->format = $default_format;
+
+					}*/
+					else if($field_type == "entity_reference")
+					{
+						$settings = $field_definition->getSettings();
+						$target_entity_type = $settings['target_type'];
+
+						if($target_entity_type == "node")
+						{
+							$bundles = $settings["handler_settings"]["target_bundles"];
+							$refIds = $this->getNodeIds($value,$bundles);
+
+							$node->get($field_name)->setValue($refIds);
+						}
+						else if($target_entity_type == "taxonomy_term"){
+							$bundles = $settings["handler_settings"]["target_bundles"];
+							$refIds = $this->getTermIds($value,$bundles);
+
+							$node->get($field_name)->setValue($refIds);
+						}
+						else if($target_entity_type == "user"){
+							$refIds = $this->getUserIds($value);
+
+							$node->get($field_name)->setValue($refIds);
+						}
+						else if($target_entity_type == "media"){
+							$refIds = $this->process_media_string($value);
+							$node->get($field_name)->setValue($refIds);
+						}
+						
+						//dump($settings);
+						//dump($target_entity_type);
+						//dump($value);
+
+						
+					}
+					else{
+						if($field_type == "decimal")
+						{
+							foreach ($allValues as &$value) {
+								$value = floatval($value);
+							}
+						}
+						$node->get($field_name)->setValue($allValues);
+					}
+
+					
+				}
+			}
+
+			if(isset($values["metatag"]) && trim($values["metatag"])!='')
+			{
+				$parts = explode('<SEPERATOR>', $values["metatag"]);
+
+				$metaValues = array();
+
+				// Iterate through each part
+				foreach ($parts as $part) {
+					// Split each part into key and value using colon
+					$keyValue = explode(":", $part, 2);
+					
+					if (count($keyValue) === 2) {
+						$key = trim($keyValue[0]);
+						$value = trim($keyValue[1]);
+						if($key!='' && in_array($key,["title","description","keywords"])){
+							$metaValues[$key] = $value;
+						}
+					}
+				}
+				if(!empty($metaValues))
+				{
+					$node->set("field_meta_tags",serialize($metaValues));
+				}
+			}
+			
+			$node->save();
+			
+			if(isset($values["path"]) && trim($values["path"])!='')
+			{
+				try{
+					$path_alias = PathAlias::create([
+						'path' => '/node/' . $node->id(),
+						'alias' => $values["path"],
+					]);
+					$path_alias->save();
+				}
+				catch(Exception $e){}
+				
+			}
+			
+			//die("Testing");
+
+		}
+		$context['results'][] = $index;
+    	$context['message'] = t('Processed chunk @index', ['@index' => $index]);
+	}
+
+	public function importFinished($success, $results, $operations) {
+		$msgObj = $this->msgObj;
+		if ($success) {
+		  $msgObj->addMessage(t("Data imported successfully !!"));
+		}
+		else {
+		  $msgObj->addError(t("Data import has some issue!!"));
+		  
+		}
+	}
+
+	public function getNodeIds($allValues, $bundles) {
+		$node_ids = [];
+	
+		// Split the title values into an array.
+		$titles = explode('<SEPERATOR>', $allValues);
+	
+		// Query nodes based on title and content type.
+		$query = $this->entityTypeManager->getStorage('node')->getQuery();
+		$query->condition('type', $bundles, 'IN');
+		$query->condition('title', $titles, 'IN');
+		$query->accessCheck(FALSE);
+
+		$node_ids = $query->execute();
+	
+		return $node_ids;
+	}
+	public function getTermIds($allValues, $vocabularies) {
+		$term_ids = [];
+	
+		// Split the title values into an array.
+		$titles = explode('<SEPERATOR>', $allValues);
+	
+		// Query terms based on title and vocabulary.
+		$query = $this->entityTypeManager->getStorage('taxonomy_term')->getQuery();
+		$query->condition('vid', $vocabularies, 'IN');
+		$query->condition('name', $titles, 'IN');
+		$query->accessCheck(FALSE);
+		$term_ids = $query->execute();
+		
+		return $term_ids;
+	}
+
+	public function getUserIds($allValues) {
+		$user_ids = [];
+	
+		// Split the username values into an array.
+		$usernames = explode('<SEPERATOR>', $allValues);
+	
+		// Query users based on username and roles.
+		$query = $this->entityTypeManager->getStorage('user')->getQuery();
+		$query->condition('name', $usernames, 'IN');
+		$query->accessCheck(FALSE); // Bypass access checks.
+		$user_ids = $query->execute();
+		
+		return $user_ids;
+	  }
+
+	protected function processCSVFile($file_path) {
+		$csv_data = [];
+	
+		$csvFile = fopen($file_path, 'r');
+		if ($csvFile) {
+		  $keys = fgetcsv($csvFile);	
+		  while ($row = fgetcsv($csvFile)) {
+			$rowData = array_combine($keys, $row);
+			$csv_data[] = $rowData;
+		  }
+		  fclose($csvFile);
+		}
+	
+		return $csv_data;
+	  
+	}
+	public function getDirectoryName($directory_pattern)
+	{
+		$contains_tokens = $this->containsTokens($directory_pattern);
+		
+		if ($contains_tokens) {
+			$token_service = \Drupal::token();
+
+			// Replace tokens in the directory pattern.
+			$directory = $token_service->replace($directory_pattern, [], ['clear' => TRUE]);
+			/*
+			echo "directory: ";
+			die(dump($directory));
+			
+			$bubbleable_metadata = new BubbleableMetadata();
+			$bubbleable_metadata->addCacheContexts(['url.query_args']);
+			$bubbleable_metadata->applyTo([$directory]);
+			*/
+		} else {
+			$directory = $directory_pattern;
+		}
+
+		return $directory;
+	}
+
+	public function containsTokens($string) {
+		return preg_match('/\[[a-zA-Z:_]+\]/', $string) === 1;
+	}
+
+	public function process_media_string($input_string) {
+		$media_data = [];
+		$media_ids = [];
+	  
+		$media_entries = explode('<SEPERATOR>', $input_string);
+		
+		foreach ($media_entries as $entry) {
+		  list($type, $url) = explode('::::', $entry);
+		  $media_data[$type] = $url;
+		}
+		
+		foreach ($media_data as $type => $url) {
+		  // Check if the URL is already associated with a media entity.
+
+		  //$media = $this->get_media_by_url($url);
+		  $media = NULL;
+	  
+		  if (!$media) {
+			// If not, create a new media entity and associate it with the URL.
+			$media = $this->create_media_from_url($url, $type);
+		  }
+	  
+		  if ($media) {
+			$media_ids[] = $media->id();
+		  }
+		}
+	  
+		return $media_ids;
+	  }
+	  
+	  public function get_media_by_url($url) {
+		$query = \Drupal::entityQuery('media')
+			->condition('field_media_file.uri', $url)
+			->range(0, 1);
+		
+		$query->accessCheck(FALSE);
+		$media_ids = $query->execute();
+		//echo "Media Ids: ";
+		//die(dump($media_ids));
+
+		if (!empty($media_ids)) {
+			$media_id = reset($media_ids);
+			return Media::load($media_id);
+		}
+
+		return NULL;
+	  }
+	  
+	  public function create_media_from_url($url, $type) {
+		$file_url = $url;
+		$urlParts = parse_url($url);
+
+		// Get the path from the URL
+		$path = $urlParts['path'];
+
+		// Extract the image filename
+		$filename = basename($path);
+
+		//echo "File Name: ".$filename."<hr/>"; 
+		//$filename = date("YmdHis");
+		if($type != "remote_video")
+		{
+			// Create a new file entity from the remote URL.
+			$base_url = \Drupal::request()->getSchemeAndHttpHost();
+			//if (strpos($file_url, 'http://') !== 0 && strpos($file_url, 'https://') !== 0) {
+				//$file_url = $base_url.$file_url;
+			//}
+			$drupalRoot = \Drupal::root()."/";
+			$file_data = file_get_contents($file_url);
+
+			//dump($file_data);
+			//echo $file_url."<hr/>";
+			
+			file_put_contents($drupalRoot."sites/default/files/".$filename, $file_data);
+								
+			$destination = 'public://'.$filename;
+								
+			//$this->filesystem->move($drupalRoot."sites/default/files/".$filename, $destination, FileSystemInterface::EXISTS_REPLACE);
+
+			// Create a file entity from the downloaded image
+			$file = \Drupal::entityTypeManager()->getStorage('file')->create([
+				'uri' => $destination,
+			]);
+
+			$file->save();
+
+			//echo $file->id()."<hr/>";
+			if($type=="image"){  $targetField = "field_media_image"; }
+			else if($type=="document"){  $targetField = "field_media_document"; }
+			else if($type=="video"){  $targetField = "field_media_video_file"; }
+			else if($type=="audio"){  $targetField = "field_media_audio_file"; }
+			else if($type=="remote_video"){  $targetField = "field_media_document"; }
+
+			$media = Media::create([
+				'bundle' => $type,
+				'name' => $filename,
+				$targetField => [
+				  'target_id' => $file->id(),
+				],
+			  ]);
+			
+		}
+		else{
+			$filename = date("YmdHis");
+			$media = Media::create([
+				'bundle' => $type,
+				'name' => $filename,
+				'field_media_oembed_video' => $url, // Adjust the field name accordingly.
+			]);
+		}
+		$media->save();
+		
+		return $media;
+	  }
+	
+}
---
+++ content_migration_ia/content_migration_ia/src/Form/MigrationForm.php
@@ -0,0 +1,260 @@
+<?php
+
+
+namespace Drupal\content_migration_ia\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\file\Entity\File;
+use Drupal\taxonomy\Entity\Term;
+use Drupal\Core\Url;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\media\Entity\Media;
+
+/**
+ * Migrate CSV form
+ */
+
+class MigrationForm extends FormBase
+{
+
+	protected $msgObj, $filesystem, $database, $languageManager;
+	protected $entityTypeManager, $file_url_generator;
+
+	public function __construct()
+	{
+		$this->msgObj = \Drupal::messenger();
+		$this->filesystem = \Drupal::service("file_system");
+		$this->database = \Drupal::database();
+		$this->entityTypeManager = \Drupal::service('entity_type.manager');
+		$this->file_url_generator = \Drupal::service('file_url_generator');
+		$this->languageManager = \Drupal::service("language_manager");
+	}
+
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getFormId()
+	{
+		return 'migration_form';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function buildForm(array $form, FormStateInterface $form_state)
+	{
+
+		$content_type_storage = $this->entityTypeManager->getStorage('node_type');
+		$content_type_entities = $content_type_storage->loadMultiple();
+
+		$content_types = ['' => "Select Content Type"];
+		foreach ($content_type_entities as $content_type) {
+			$content_types[$content_type->id()] = $content_type->label();
+		}
+		if (isset($_GET['debug'])) {
+			die(dump($content_types));
+		}
+
+		$form['contenttype'] = [
+			'#type' => "select",
+			'#title' => 'Content Type',
+			'#required' => true,
+			'#description' => 'Select content type where you want to import the csv data',
+			'#options' => $content_types,
+		];
+
+		$form["#attributes"] = ['enctype' => 'multipart/form-data'];
+
+
+		$form['submit'] = [
+			'#type' => "submit",
+			"#value" => "Export",
+		];
+
+
+		return $form;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function submitForm(array &$form, FormStateInterface $form_state)
+	{
+
+		$content_type = $form_state->getValue('contenttype', '');
+
+		$msgObj = $this->msgObj;
+
+		//echo "<pre>"; print_r($_POST); exit;
+
+		if ($content_type == "") {
+			$msgObj->addError(t("Content type is not available"));
+		} else {
+
+			$nodes = $this->entityTypeManager->getStorage('node')
+				->loadByProperties(['type' => $content_type]);
+
+      if(!empty($nodes))
+      {
+			  $output = [];
+			  foreach ($nodes as $node) {
+				  $output[] = $this->prepareNodeData($node);
+
+			  }
+      }else{
+        
+        $field_definitions = \Drupal::entityTypeManager()->getStorage('field_config')->loadByProperties(['bundle' => $content_type]);
+
+        $output[0]["nid"] = '0';
+        $output[0]["langcode"] = 'en';
+        $output[0]["title"] = '';
+        $output[0]["path"] = '';
+        foreach($field_definitions as $field_definition)
+        {
+          if ($field_definition instanceof \Drupal\field\Entity\FieldConfig) {
+               $field_name = $field_definition->getName();
+               $output[0][$field_name] =  "";
+          }
+          
+        }
+
+      }
+
+      
+			//echo "Output: ";
+			//die(dump($output));
+
+			$filename = $content_type . ".csv";
+			$this->arrayToCsv($output, $filename);
+		}
+
+
+		$messages = $msgObj->all();
+
+		if (!isset($messages['error'])) {
+			$msgObj->addMessage(t("Data exported successfully !!"));
+		}
+	}
+
+	public function arrayToCsv($data, $filename = 'output.csv')
+	{
+    header('Content-Type: text/csv');
+		header('Content-Disposition: attachment; filename="' . $filename . '"');
+
+		$output = fopen('php://output', 'w');
+
+		// Write the headings from array keys.
+		fputcsv($output, array_keys(reset($data)));
+
+		// Write the data rows.
+		foreach ($data as $row) {
+			fputcsv($output, $row);
+		}
+
+		fclose($output);
+		exit;
+	}
+
+
+	public function prepareNodeData($node)
+	{
+		$data = $node->toArray();
+
+		$search = ["uuid", "vid", "type", "status", "uid", "created", "changed", "promote", "sticky", "default_langcode", "menu_link", "comment", "content_translation_source", "content_translation_outdated"];
+
+		$finalData = [];
+		$i = 0;
+
+		foreach ($data as $field_name => &$field) {
+
+			if (stristr($field_name, "revision")) {
+				unset($data[$field_name]);
+				continue;
+			} else if (in_array($field_name, $search)) {
+				unset($data[$field_name]);
+				continue;
+			}
+			//echo $field_name."<br/>";
+			if ($node->hasField($field_name)) {
+				$field_definition = $node->getFieldDefinition($field_name);
+				$field_type = $field_definition->getType();
+				$settings = $field_definition->getSettings();
+				$target_entity_type = $settings['target_type'];
+				$finalVal = [];
+				if ($field_type == "entity_reference") {
+					$tags = $node->get($field_name)->referencedEntities();
+					if ($target_entity_type == "media") {
+						foreach ($tags as $media_item) {
+							// Load the referenced media entity.
+							$media = Media::load($media_item->id());
+
+							if ($media) {
+								$media_type = $media->bundle(); // Get the media type machine name.
+								$file_url = '';
+								if ($media_type == 'image') {
+									$file_url = $media->field_media_image->entity->createFileUrl();
+								} elseif ($media_type == 'document') {
+									$file_url = $media->field_media_document->entity->createFileUrl();
+								} elseif ($media_type == 'video') {
+									//die(dump($media));
+									$video_file = $media->get('field_media_video_file')->entity;
+									if ($video_file instanceof File) {
+										$file_url = $media->field_media_video_file->entity->createFileUrl();
+									} else {
+										$file_url = $media->get('field_media_video_file')->getString();
+									}
+								} else if ($media_type == "remote_video") {
+									$file_url = $media->get('field_media_oembed_video')->getString();
+								} else if ($media_type == "audio") {
+									$file_url = $media->field_media_audio->entity->createFileUrl();
+								}
+
+								$finalVal[] = $media_type . "::::" . $file_url;
+							}
+						}
+					} else {
+						foreach ($tags as $tag) {
+							$finalVal[] = $tag->label();
+						}
+					}
+				} else {
+					$values = $node->{$field_name}->getValue();
+					foreach ($values as $value) {
+						if ($field_type == "image" || $field_type == 'file') {
+							$file = \Drupal\file\Entity\File::load($value['target_id']);
+							if ($file) {
+								$url = $this->file_url_generator->generateAbsoluteString($file->getFileUri());
+
+								$finalVal[] = $url;
+							}
+						} else if ($field_type == "path") {
+							$finalVal[] = isset($value["alias"]) ? $value["alias"] : '';
+						} else if ($field_type == "metatag_computed" && 0) {
+							//die(dump($values));
+							$finalVal[] = $value["attributes"]["name"] . ":" . $value["attributes"]["content"];
+
+						} else if ($field_type == "float") {
+							$finalVal[] = (float) $value["value"];
+						} else {
+							$finalVal[] = $value["value"];
+						}
+					}
+				}
+
+				//$finalData[$field_name." -- ".$field_type] = implode("<SEPERATOR>",$finalVal);
+				$finalData[$field_name] = implode("<SEPERATOR>", $finalVal);
+
+			}
+			$i++;
+		}
+    if(isset($finalData["field_meta_tags"]))
+    {
+      unset($finalData["metatag"]);
+    }
+    //print_r($finalData); exit;
+		return $finalData;
+	}
+
+}
